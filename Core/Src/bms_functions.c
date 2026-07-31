f043408a8ce1255db064bae002ca05679776bf61
Samuel Petrovic <smpetrovic131@gmail.com>
2026-07-31 13:29:45 +0800

    IC 5 temp 0 bypassed for testing

--- Core/Src/bms_functions.c
+++ Core/Src/bms_functions.c
@@ -291,7 +291,9 @@ bool check_ut_ot_fault(uint8_t total_ic, uint16_t temps[TOTAL_IC][TEMPS_PER_IC],
         for(uint8_t ch = 1; ch < TEMPS_PER_IC; ch++)
         {
 
-            if(temps[ic_idx][ch] > ut)
+            if(ic_idx == 5 && ch = 1) continue;
+
+        	if(temps[ic_idx][ch] > ut)
             {
                 *mask |= FAULT_UNDERTEMP;
                 fault = true;
